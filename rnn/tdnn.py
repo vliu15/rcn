9a92297e11fc1ecf452bdb900798c61cb3b51be6
Vincent Liu <vliu15@stanford.edu>
2018-12-20 23:47:24 -0800

    comments for tdnn

--- rnn/tdnn.py
+++ rnn/tdnn.py
@@ -28,22 +28,30 @@ class TimeDelayNeuralNetwork(object):
         layers = tdnn_params["layers"]
         kernel_initializer = tdnn_params["kernel_initializer"]
         bias_initializer = tdnn_params["bias_initializer"]
-        # initialize time series
+
+        # width: length of memory
+        # width is determined from window sizes and number of hidden layers
+        # so that after all convolutions, the output will be the action vector
         width = 1
         for _ in range(len(layers) + 1):
             width = (width - 1) * self.stride + self.window
+        # initialize time series
+        # stores sequence of observations
         self.series = np.zeros(shape=(width, input_size))
+
         # initialize convolution kernels
         inp_size = input_size
         layers.append(output_size)
         self.kernels, self.biases = [], []
+        # each kernel reduces length of time series (no padding) and changes
+        # the size of information channels (i.e. observation size)
         for out_size in layers:
             self.kernels.append(kernel_initializer(shape=(out_size, self.window, inp_size)))
             self.biases.append(bias_initializer(shape=(out_size)))
             inp_size = out_size
 
     def predict(self, inp):
-        # adjust time series
+        # update time series with new observation
         ser = np.copy(self.series)
         ser[:-1] = np.copy(self.series[1:])
         ser[-1] = inp.flatten()
@@ -52,9 +60,12 @@ class TimeDelayNeuralNetwork(object):
         # convolve as time delay
         conv_out = ser
         for k, b in zip(self.kernels, self.biases):
+            # output of convolution is a shortened time length and hidden
+            # size of output channels
             conv_out = convolve(conv_out, k, b, self.stride, self.window)
             conv_out = self.layer_activation(conv_out)
         
+        # initial width should be perfectly reduced to a time series of length 1
         return conv_out
 
     def get_weights(self):
--- utils/layers.py
+++ utils/layers.py
@@ -14,9 +14,13 @@ def convolve(self, series, kernel, bias, stride, window):
     """
     i = 0
     conv_out = []
+    # compute scalar output of dot product at each window
     while i + window <= series.shape[0]:
+        # take current window
         inp = series[i:i+window]
+        # find dot product of kernel and time series window + bias
         out = np.sum(np.sum(np.expand_dims(inp, axis=0) * kernel, axis=-1), axis=-1) + bias
+        # append scalar to list to cast to array later
         conv_out.append(out)
         i += stride
     return np.array(conv_out)
