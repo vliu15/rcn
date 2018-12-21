689f3d5fe3cf2c8c56d2f8884d45639de78c9d3a
Vincent Liu <vliu15@stanford.edu>
2018-12-21 15:36:26 -0800

    revise initializers

--- utils/initializers.py
+++ utils/initializers.py
@@ -3,7 +3,26 @@ File for custom initializers.
 """
 import numpy as np
 
-def gaussian(mu=0, sigma=1):
+def gaussian(mu=0, sigma=1e-3):
+    """
+    Returns Gaussian initializer from distribution ~ N(mu, sigma^2)
+    """
     def f(shape):
-        return sigma * np.random.randn(*shape) - mu
+        return sigma * np.random.randn(*shape) + mu
+    return f
+
+def uniform(scale=1e-3):
+    """
+    Returns uniform initializer from distribution ~ U[0, scale)
+    """
+    def f(shape):
+        return scale * np.random.uniform(size=shape)
+    return f
+
+def constant(fill=1e-3):
+    """
+    Returns constant initializer with value fill
+    """
+    def f(shape):
+        return fill * np.ones(shape=shape)
     return f
